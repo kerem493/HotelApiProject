a39757b0cc7224743d73e1aeb8f2ac6cb2a13cce
Kerem KORKMAZ <kerem.493@hotmail.com>
2023-07-14 12:25:31 +0300

    ...
    ....

--- Kaynak kodlar/MyUdemyProject-master/Frontend/HotelProject.WebUI/ViewComponents/Default/_TrailerPartial.cs
+++
@@ -1,14 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-namespace HotelProject.WebUI.ViewComponents.Default
-{
-    public class _TrailerPartial : ViewComponent
-    {
-        public IViewComponentResult Invoke()
-        {
-            return View();
-        }    }
-}
